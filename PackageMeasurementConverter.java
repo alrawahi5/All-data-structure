d94910b9b20f367684fe960c238783850778ed6a
ABDULLAH <abdullah555523@hotmail.com>
2024-03-20 11:55:07 +0400

    added the proper linkage between the letters and numbers

--- PackageMeasurementConverter.java
+++ PackageMeasurementConverter.java
@@ -14,21 +14,21 @@ public class PackageMeasurementConverter {
         for (i  = 0; i < alphabets.length(); i++){
             letters[i] = alphabets.charAt(i);
             count[i] = countSupport++;
-            System.out.println(letters[i] + " = " + count[i]);
+            //System.out.println(letters[i] + " = " + count[i]); // letter == number
+
         }
 
-        System.out.println(Arrays.toString(letters));
-        System.out.println(Arrays.toString(count));
+        //test
+        //System.out.println(Arrays.toString(letters));
+        //System.out.println(Arrays.toString(count));
             System.out.println("Enter the string that you want to evaluate: ");
             String evaluatedString = sc.nextLine();
-            for (int j = 0; j<evaluatedString.length()-1; j++){
-                if (letters[j] == letters[count[j]]){
-                    System.out.println("[" + count + "]");
+        System.out.print("[");
+            for (int j = 0; j<evaluatedString.length(); j++){
+                if (Character.isAlphabetic(evaluatedString.charAt(j)) && evaluatedString.charAt(j) == letters[Character.toLowerCase(evaluatedString.charAt(j)) - 'a']) {
+                    System.out.print(count[Character.toLowerCase(evaluatedString.charAt(j)) - 'a'] + ",");
                 }
-                System.out.println("[" + count + "]");
             }
-
-
-
+        System.out.println("]");
     }
 }
